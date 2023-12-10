e1b065c892832f6158024abc10bb6e18e5a85d7b
bryan-b <barryb.ebeh@gmail.com>
2023-12-10 17:31:35 +0100

    Delete run.sh script and related files

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo cp utils.sh jq odogwu /usr/local/bin/odogwu
+sudo chmod +x /usr/local/bin/odogwu/utils.sh /usr/local/bin/odogwu/odogwu /usr/local/bin/odogwu/jq
+
+echo "Odogwu has been installed successfully. Type 'odogwu' to get started."
