2bf0e855d99472674262e1149020db81f6028184
bryan-b <barryb.ebeh@gmail.com>
2023-12-10 18:10:56 +0100

    Add installation script for Odogwu

--- install.sh
+++ install.sh
@@ -1,4 +1,26 @@
-sudo cp utils.sh jq odogwu /usr/local/bin/odogwu
-sudo chmod +x /usr/local/bin/odogwu/utils.sh /usr/local/bin/odogwu/odogwu /usr/local/bin/odogwu/jq
+create_dir() {
+    if [ ! -d /usr/local/bin/odogwu ]; then
+        sudo mkdir /usr/local/bin/odogwu
+    fi
+}
 
-echo "Odogwu has been installed successfully. Type 'odogwu' to get started."
+replace_all() {
+    sudo rm -rf /usr/local/bin/odogwu/*
+    sudo mv * /usr/local/bin/odogwu
+}
+
+permit() {
+    sudo chmod +x /usr/local/bin/odogwu/odogwu
+    sudo chmod +x /usr/local/bin/odogwu/utils.sh
+    sudo chmod +x /usr/local/bin/odogwu/install.sh
+
+    sudo ln -s /usr/local/bin/odogwu/odogwu /usr/local/bin/odogwu
+
+    echo "Odogwu installed successfully!"
+    echo "=============================="
+    echo "Run 'odogwu' to get started"
+}
+
+create_dir
+replace_all
+permit
