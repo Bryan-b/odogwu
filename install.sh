1485adcabf8405623670db90640260fc3f04311a
bryan-b <barryb.ebeh@gmail.com>
2023-12-11 03:50:39 +0100

    Refactor install.sh script to support different shell environments

--- install.sh
+++ install.sh
@@ -18,39 +18,25 @@ setup() {
 
     sudo ln -s /usr/local/bin/odogwu/odogwu.sh /usr/local/bin/odogwu
 
-
-    echo "=============================="
-    echo "Select your shell environment"
-    echo "=============================="
-    echo "1. Bash"
-    echo "2. Zsh"
-    echo "3. Bash Profile"
-    echo "=============================="
-    read -p "Enter your choice: " choice
-
-    # Check if $choice is empty or not an integer
-    if [ -z "$choice" ] || ! [[ "$choice" =~ ^[0-9]+$ ]]; then
-        echo "Invalid choice"
-        exit 1
-    fi
-
-    if [ "$choice" -eq 1 ]; then
-        echo "export PATH=$PATH:/usr/local/bin/odogwu/odogwu.sh" >> ~/.bashrc
-        alias odogwu='bash /usr/local/bin/odogwu/odogwu.sh'
-        source ~/.bashrc
-    elif [ "$choice" -eq 2 ]; then
+    if [ "$(uname)" == "Darwin" ]; then
+        # add to .zshrc and .bashrc
         echo "export PATH=$PATH:/usr/local/bin/odogwu/odogwu.sh" >> ~/.zshrc
-        alias odogwu='bash /usr/local/bin/odogwu/odogwu.sh'
+        echo "export PATH=$PATH:/usr/local/bin/odogwu/odogwu.sh" >> ~/.bashrc
+        echo alias odogwu='bash /usr/local/bin/odogwu/odogwu.sh' >> ~/.zshrc
+        echo alias odogwu='bash /usr/local/bin/odogwu/odogwu.sh' >> ~/.bashrc
         source ~/.zshrc
-    elif [ "$choice" -eq 3 ]; then
+        source ~/.bashrc
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        echo "export PATH=$PATH:/usr/local/bin/odogwu/odogwu.sh" >> ~/.bashrc
         echo "export PATH=$PATH:/usr/local/bin/odogwu/odogwu.sh" >> ~/.bash_profile
-        alias odogwu='bash /usr/local/bin/odogwu/odogwu.sh'
+        echo alias odogwu='bash /usr/local/bin/odogwu/odogwu.sh' >> ~/.bashrc
+        echo alias odogwu='bash /usr/local/bin/odogwu/odogwu.sh' >> ~/.bash_profile
+        source ~/.bashrc
         source ~/.bash_profile
     else
         echo "Invalid choice"
         exit 1
     fi
-
 
     echo "Odogwu installed successfully!"
     echo "=============================="
