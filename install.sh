b328c71a54523bdde01e730e2cd8baf4d6b83c68
bryan-b <barryb.ebeh@gmail.com>
2023-12-11 04:14:40 +0100

    Update install script to add odogwu to shell configuration files

--- install.sh
+++ install.sh
@@ -19,18 +19,27 @@ setup() {
     sudo ln -s /usr/local/bin/odogwu/odogwu.sh /usr/local/bin/odogwu
 
     which_shell=$(echo $SHELL)
-    
+
     if [ $which_shell = "/bin/zsh" ]; then
+        sed -i '' '/PATH=$PATH:\/usr\/local\/bin\/odogwu/d' ~/.zshrc
+        sed -i '' '/alias odogwu="\/usr\/local\/bin\/odogwu\/odogwu.sh"/d' ~/.zshrc
+
         echo "PATH=$PATH:/usr/local/bin/odogwu" >> ~/.zshrc
         echo alias odogwu="bash /usr/local/bin/odogwu/odogwu.sh" >> ~/.zshrc
         echo "source /usr/local/bin/odogwu/odogwu.sh" >> ~/.zshrc
     elif [ $which_shell = "/bin/bash" ]; then
+        sed -i '' '/PATH=$PATH:\/usr\/local\/bin\/odogwu/d' ~/.bashrc
+        sed -i '' '/alias odogwu="\/usr\/local\/bin\/odogwu\/odogwu.sh"/d' ~/.bashrc
+
         echo "PATH=$PATH:/usr/local/bin/odogwu" >> ~/.bashrc
         echo alias odogwu="bash /usr/local/bin/odogwu/odogwu.sh" >> ~/.bashrc
         echo "source /usr/local/bin/odogwu/odogwu.sh" >> ~/.bashrc
     else
+        sed -i '' '/PATH=$PATH:\/usr\/local\/bin\/odogwu/d' ~/.bash_profile
+        sed -i '' '/alias odogwu="\/usr\/local\/bin\/odogwu\/odogwu.sh"/d' ~/.bash_profile
+        
         echo "PATH=$PATH:/usr/local/bin/odogwu" >> ~/.bash_profile
-        echo alias odogwu="bash /usr/local/bin/odogwu/odogwu.sh" >> ~/.bash_profile
+        echo alias odogwu="/usr/local/bin/odogwu/odogwu.sh" >> ~/.bash_profile
         echo "source /usr/local/bin/odogwu/odogwu.sh" >> ~/.bash_profile
     fi
 
